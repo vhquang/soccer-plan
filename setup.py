e9c4a2a5ec1a828f9e1e79421936d76350e04cbd
Quang Vu <vhquang@gmail.com>
2017-10-01 23:46:25 -0700

    use setuptools instead of distutils for packaging the script

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='Soccer Strategy',
+setup(name='soccer_strategy',
       version='0.0.1',
       description='A visual demonstration for soccer strategy',
       author='Thanh Vu',
       author_email='',
-      url='',
-      packages=[],
+      url='https://github.com/thanhvu262/soccer-plan',
+      license='MIT',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+
+          'Programming Language :: Python :: 3.5',
+      ],
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+      python_requires='>=3.4',
      )
