f04949a939e690cd0e290ce108e51409276840b8
Quang Vu <vhquang@gmail.com>
2017-10-01 22:52:35 -0700

    add setup file for setting Travis

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Soccer Strategy',
+      version='0.0.1',
+      description='A visual demonstration for soccer strategy',
+      author='Thanh Vu',
+      author_email='',
+      url='',
+      packages=[],
+     )
